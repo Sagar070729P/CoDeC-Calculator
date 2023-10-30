d044c7725d2c520b145e00432075071a24f93e3b
Sagar070729P <149380671+Sagar070729P@users.noreply.github.com>
2023-10-30 18:58:28 +0530

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -8,7 +8,7 @@ double addition(double num1, double num2) {
 
 // Function to subtract two numbers
 double subtraction(double num1, double num2) {
-    return num1 - num2;
+    return num1 + num2;
 }
 
 // Function to divide two numbers, with error handling for division by zero
